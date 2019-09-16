bbe67373ff116c3ea7bac4814eac4b0a3f9de42f
Will Daskam <willd2122@gmail.com>
2019-09-16 22:19:30 +0000

    Completed luhn algorithm

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -2,23 +2,22 @@ module Luhn
   def self.is_valid?(number)
     array = number.to_s.chars.map(&:to_i).reverse    
     i = 1
-    b = 0
-    array.each do | x |
-      c = x
+    value_sum = 0
+    array.each do | value |
+      new_number = value
       if i % 2 == 0
-        c = x * 2
+        new_number = value * 2
       end
-      if c >= 10
-        c = c - 9
+      if new_number >= 10
+        new_number = new_number - 9
       end
-      b = b + c
+      value_sum = value_sum + new_number
       i = i + 1
       end
-      if b % 10 == 0 
+      if value_sum % 10 == 0 
         return true
       else 
         return false
-      end
-      
+      end 
   end
 end
